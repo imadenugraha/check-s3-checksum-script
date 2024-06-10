f953f99fd1a59e072a62507f2fea1bed81ae5c20
imadenugraha <imadenugraha@gmail.com>
2024-06-10 22:46:48 +0700

    feat(upload): create upload_object function to upload the object

---
+++ check_s3_checksum/upload_object.py
@@ -0,0 +1,36 @@
+import logging
+import os
+import hashlib
+
+from check_s3_checksum.create_session import create_session
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def upload_object(file_path: str, object_key: str):
+    load_dotenv('.env')
+
+    bucket = os.getenv('AWS_BUCKET_NAME')
+
+    s3 = create_session().resource('s3')
+
+    md5 = hashlib.md5()
+
+    with open(file_path, 'rb') as f:
+        while chunk := f.read(8192):
+            md5.update(chunk)
+    
+    md5_file = md5.hexdigest()
+
+    try:
+        with open(file_path, 'rb') as f_data:
+            response = s3.Object(bucket_name=bucket, key=object_key).put(Body=f_data, ContentMD5=md5_file)
+
+        return response
+    except Exception as e:
+        logging.error(e)
+        print(f'ERROR: {e}')
+        return None
