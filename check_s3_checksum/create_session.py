1f2b14d19bb211c760ff7d5f6aa81957c1e14e52
imadenugraha <imadenugraha@gmail.com>
2024-06-10 16:45:01 +0700

    feat(session): create aws session

---
+++ check_s3_checksum/create_session.py
@@ -0,0 +1,36 @@
+from boto3 import Session
+from botocore.exceptions import NoCredentialsError,PartialCredentialsError,EndpointConnectionError
+from dotenv import load_dotenv
+from os import getenv
+import logging
+
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def create_session():
+    load_dotenv('.env')
+
+    access_key = getenv('AWS_ACCESS_KEY')
+    secret_key = getenv('AWS_SECRET_KEY')
+    region = getenv('AWS_REGION')
+    
+    if not all([access_key, secret_key, region]):
+        logging.error("Please set credential on .env file!")
+        return None
+    
+    try:
+        aws_session = Session(aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key,
+                              region_name=region)
+        return aws_session
+    except NoCredentialsError as no_cred:
+        logging.error(no_cred)
+        print('Please set the credential!')
+    except PartialCredentialsError as partial_cred_err:
+        logging.error(partial_cred_err)
+        print('Partial credential is not valid!')
+    except EndpointConnectionError as endpoint_err:
+        logging.error(endpoint_err)
+        print('Cannot connect to AWS!')
+    except Exception as err:
+        logging.error(err)
+        print(f'Something happened!: {err}')
