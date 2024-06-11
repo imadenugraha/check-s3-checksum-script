b313d14bc58b5e14ba17c4027f89afc97c95e46f
imadenugraha <imadenugraha@gmail.com>
2024-06-11 23:11:21 +0700

    refactor(checksum): change read chunk to 4096

--- check_s3_checksum/checksum_md5.py
+++ check_s3_checksum/checksum_md5.py
@@ -6,7 +6,7 @@ def calculate_md5(file_path: str) -> str:
     md5 = hashlib.md5()
     
     with open(file=file_path, mode='rb') as f:
-        while chunk := f.read(8192):
+        while chunk := f.read(4096):
             md5.update(chunk)
     
     digest = md5.digest()
