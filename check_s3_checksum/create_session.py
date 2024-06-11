50f12b20707860384b9f4d1dc3a90be4e90b016d
imadenugraha <imadenugraha@gmail.com>
2024-06-11 15:45:17 +0700

    refactor: remove unused exception and add log for crucial step

--- check_s3_checksum/create_session.py
+++ check_s3_checksum/create_session.py
@@ -2,16 +2,15 @@ import logging
 from os import getenv
 
 from boto3 import Session
-from botocore.exceptions import (
-    EndpointConnectionError,
-    NoCredentialsError,
-    PartialCredentialsError,
-)
 from dotenv import load_dotenv
 
-logging.basicConfig(
-    level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, 
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    handlers=[logging.FileHandler('output.log'),
+                              logging.StreamHandler()]
 )
+
+logger = logging.getLogger(__name__)
 
 
 def create_session():
@@ -22,26 +21,20 @@ def create_session():
     region = getenv("AWS_REGION")
 
     if not all([access_key, secret_key, region]):
-        logging.error("Please set credential on .env file!")
+        logger.error("Please set credential on .env file!")
         return None
 
     try:
+        logger.info('Creating session...')
         session = Session(
             aws_access_key_id=access_key,
             aws_secret_access_key=secret_key,
             region_name=region,
         )
+        
+        logger.info('Session has been created!')
         return session
 
-    except NoCredentialsError as no_cred:
-        logging.error(no_cred)
-        print("Please set the credential!")
-    except PartialCredentialsError as partial_cred_err:
-        logging.error(partial_cred_err)
-        print("Partial credential is not valid!")
-    except EndpointConnectionError as endpoint_err:
-        logging.error(endpoint_err)
-        print("Cannot connect to AWS!")
     except Exception as err:
-        logging.error(err)
-        print(f"Something happened!: {err}")
+        logger.error(err)
+        return None
