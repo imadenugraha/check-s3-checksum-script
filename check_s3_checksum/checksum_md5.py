be9799aa298aa62b2e0748fb6ae6b7a0e9f14f65
imadenugraha <imadenugraha@gmail.com>
2024-06-12 08:58:20 +0700

    feat(checksum): add log and try except block

--- check_s3_checksum/checksum_md5.py
+++ check_s3_checksum/checksum_md5.py
@@ -1,16 +1,30 @@
 import hashlib
 import base64
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(level)s - %(message)s',
+                    handlers=[logging.FileHandler('output.log'),
+                              logging.StreamHandler])
+
+logger = logging.getLogger(__name__)
 
 
 def calculate_md5(file_path: str) -> str:
     md5 = hashlib.md5()
     
-    with open(file=file_path, mode='rb') as f:
-        while chunk := f.read(4096):
-            md5.update(chunk)
+    try:
+        logger.info("Getting the MD5 checksum from file...")
+        with open(file=file_path, mode='rb') as f:
+            while chunk := f.read(4096):
+                md5.update(chunk)
     
-    digest = md5.digest()
+        digest = md5.digest()
     
-    md5_base64 = base64.b64encode(digest).decode('utf-8')
+        md5_base64 = base64.b64encode(digest).decode('utf-8')
     
-    return md5_base64
+        logger.info("MD5 has been received!")
+        return md5_base64
+    except IOError as io_err:
+        logger.error(io_err)
+        return None
